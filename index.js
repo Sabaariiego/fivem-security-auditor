9bb90edb5e79e696c0bc0e9993f3b6539bec5869
Sabaariiego <eric.sabariego@gmail.com>
2026-01-17 23:24:11 +0100

    Add CLI help option and usage instructions
    Added a shebang for CLI usage and implemented a --help/-h flag to display usage instructions for the FiveM Security Auditor. Also removed several old report JSON files from the reports directory.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 const path = require("path");
 const scanner = require("./src/scanner");
 const fixer = require("./src/fixer");
 
 const args = process.argv.slice(2);
+
+if (args.includes("--help") || args.includes("-h")) {
+  console.log(`
+    FiveM Security Auditor - Uso
+
+    node index.js [ruta] [flags]
+
+    Opciones:
+      [ruta]        Ruta de la carpeta de resources a escanear (default: ./resources)
+      --fix         Aplica correcciones automáticas a archivos detectados
+      --help, -h    Muestra esta ayuda
+
+    Ejemplo:
+      node index.js ./resources --fix
+    `);
+  process.exit(0);
+}
+
 const target = args[0] || "./resources";
 const fix = args.includes("--fix");
 
